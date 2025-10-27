8d238e8ba67ae485095bad09a57de0a837aeac1e
2317039-nec <2317039@nec.edu.in>
2025-10-27 20:47:56 +0530

    Refactor Calculator for non-interactive mode

--- Calculator.java
+++ Calculator.java
@@ -1,49 +1,33 @@
-import java.util.Scanner;
-
 public class Calculator {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        System.out.println("=== Simple Java Calculator ===");
 
-        if (System.console() == null) {
-            // Non-interactive mode (like Jenkins)
-            System.out.println("Running in Jenkins... using test values.");
-            double a = 10, b = 20;
-            char op = '+';
-            double result = switch (op) {
-                case '+' -> a + b;
-                case '-' -> a - b;
-                case '*' -> a * b;
-                case '/' -> b != 0 ? a / b : Double.NaN;
-                default -> Double.NaN;
-            };
-            System.out.println("Result = " + result);
-        } else {
-            System.out.println("Simple Java Calculator");
-            System.out.println("Supported operations: +, -, *, /");
-            System.out.println("Enter 'exit' to quit");
+        // Sample data (since Jenkins can't take interactive input)
+        double num1 = 10;
+        double num2 = 5;
+        char operator = '+';
+        double result = 0;
 
-            while (true) {
-                System.out.print("Enter first number: ");
-                String input = scanner.next();
-                if (input.equalsIgnoreCase("exit")) break;
-                double num1 = Double.parseDouble(input);
-
-                System.out.print("Enter operator: ");
-                char op = scanner.next().charAt(0);
-
-                System.out.print("Enter second number: ");
-                double num2 = scanner.nextDouble();
-
-                double result = switch (op) {
-                    case '+' -> num1 + num2;
-                    case '-' -> num1 - num2;
-                    case '*' -> num1 * num2;
-                    case '/' -> num2 != 0 ? num1 / num2 : Double.NaN;
-                    default -> Double.NaN;
-                };
-
-                System.out.println("Result = " + result);
-            }
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num2 != 0 ? num1 / num2 : Double.NaN;
+                break;
+            default:
+                System.out.println("Invalid operator");
+                return;
         }
+
+        System.out.println("Operation: " + num1 + " " + operator + " " + num2);
+        System.out.println("Result: " + result);
+        System.out.println("=== Calculation complete! ===");
     }
 }
